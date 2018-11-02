285128d8ef471092997d56bf048cd4606da0f62c
Danilo Oliveira <oliveirardanilo@gmail.com>
2018-11-02 13:35:33 -0300

    Exercicio 9_1_Chain_of_Responsibility

--- Exercicio 8_2_Observer/src/Noticiario.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author Danilo Rodrigues Oliveira RA: 81612248
- */
-public abstract class Noticiario {
-	
-	public abstract void notificaNoticia(String textoNoticia, int dia, int mes, String topico);
-}
---
+++ Exercicio 9_1_Chain_of_Responsibility/src/Slot.java
@@ -0,0 +1,8 @@
+/**
+ * @author Danilo Rodrigues Oliveira RA: 81612248
+ */
+
+public interface Slot {
+	public double recebeMoeda(int m);
+	public void setSlot(Slot s);
+}
